b2ab5570a49dbc59cecc30bcc4048420d0df469b
naman bhojwani <118888317+naman666999@users.noreply.github.com>
2025-11-05 03:26:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,19 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.js";
+import startupProfileRoutes from "./routes/startupProfile.js";
 
 dotenv.config();
 const app = express();
 
 
-app.use(cors());
-
+app.use(cors()); // Correct
 
 app.use(express.json());
 
 
 app.use("/", authRoutes);
-
+app.use("/api/startup-profile", startupProfileRoutes);
 
 const PORT = process.env.PORT || 5000;
 mongoose
